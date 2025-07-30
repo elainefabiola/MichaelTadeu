bbf94e994ad205f9d9c136ce6238e36ff1e63726
Elaine Soares <elaine.fabiola@gmail.com>
2025-07-30 17:13:19 -0300

    Create Main.java

---
+++ AulAula_2/Exercicios/Main.java
@@ -0,0 +1,48 @@
+/**
+* Exercício 3
+*Tema: Classe Aluno
+
+*Descrição:
+*Crie a classe Aluno com os atributos: nome, matricula e curso.
+*Crie dois construtores:
+*Um que recebe todos os atributos
+*Um que apenas define nome e curso
+*Crie um método exibirDados() para imprimir os dados do aluno
+*/
+
+ class Aluno{
+	  private String nome;
+	  private int matricula;
+	  private String curso;
+
+	  public Aluno(String nome, int matricula, String curso){
+	  	this.nome = nome;
+	  	this.matricula = matricula;
+	  	this. curso = curso;
+	  }
+	  public Aluno(String nome){
+	  	this.nome = nome;
+	  	matricula = 0;
+	  	curso = "Desconhecido";
+	  }
+	  public void exibirDados(){
+	  	System.out.println("Exiber Dados");
+	  	System.out.println("Nome: "+ nome);
+	  	System.out.println("Matricula: "+ matricula);
+	  	System.out.println("curso: "+ curso);
+	  }
+}
+ public class Main{
+ public static void main(String[] args){
+     
+     Aluno aluno1 = new Aluno("Elaine", 8546, "Ciência da Computação");
+     Aluno aluno2 = new Aluno("Paulo");
+
+     System.out.println("Aluno 1");
+     aluno1.exibirDados();
+     System.out.println("Aluno 2");
+     aluno2.exibirDados();
+    
+
+ 	}
+ }
